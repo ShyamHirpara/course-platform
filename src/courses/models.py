5900595e8c10960a6061fddb60b347a18e79b6ae
Shyam Hirpara <hiraparashyam666@gmail.com>
2025-12-29 00:44:17 +0530

    defined course model

---
+++ src/courses/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+Courses:
+    Title
+    Description
+    Image
+    Access
+        anyone
+        email required
+        purchase required 
+        user required
+    Status:
+        published
+        draft
+        coming soon
+
+"""
+
+"""
+Lessons:
+        Title
+        Description
+        Video
+        Status:same as course
+"""
+
+class PublishedStatus(models.TextChoices):
+    PUBLISHED = "publish",  "Published"
+    COMING_SOON = "soon" , "Coming Soon"
+    DRAFT = "draft" , "Draft"
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any" , "Anyone"
+    EMAIL_REQUIRED = "email",  "Email required"
+
+class Course(models.Model):
+    pass
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=300,blank = True,null = True)
+    # image = 
+    access = models.CharField(
+        max_length= 5,
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length= 10,
+        choices=PublishedStatus.choices,
+        default=PublishedStatus.DRAFT
+    )
+
+    @property
+    def is_published(self):
+        return self.status == PublishedStatus.PUBLISHED
